901ed4b6ef94facb8ead2dfd3582d2502a19a60d
jobsontoscano <jjortb@gmail.com>
2017-10-25 23:23:28 -0200

    atualização no front-end do game play_min

--- Play_min/index.js
+++ Play_min/index.js
@@ -4,7 +4,7 @@ var colors = ['green', 'black', 'red', 'purple'];
 
 $("#start").on("click", function(){
 		var dificuldade = parseInt($("#dificuldade").val());
-		setInterval(function() {
+		var jogo = setInterval(function() {
 			var idx = parseInt(Math.random() * 4) % 4;
 			var color = colors[idx];
 			var id = color + '-' + new Date().getTime();
@@ -16,9 +16,41 @@ $("#start").on("click", function(){
 				}
 			})(id), dificuldade);
 		}, dificuldade);
+	$("#parar").on("click", function(){
+		clearInterval(jogo);
+		$("#start").css("display", "block");
+		$("#parar").css("display", "none");
+
+		// Gerar O menu novamente
+		$("#saiba").css("display", "block");
+		$("#equipe").css("display", "block");
+		// Finalizando o menu
+		$("#contador").css("display", "none");
+	});
+
+		// $("#parar").css("display", "block");
+		// Comandos para retirar os menus
+		$("#saiba").css("display", "none");
+		$("#equipe").css("display", "none");
+		$("#tutorial").css("display", "none");
+		$("#sobre").css("display", "none");
+		$("#contador").css("display", "block");
+		// Finalizando a parte do menu
+
+		$("#parar").css("display", "block");
 		$("#start").css("display", "none");
 		$("#dificuldade").css("display", "none");
 });
+	// Funcionalidades nos supostos menus
+	$("#saiba").on("click", function(){
+		$("#tutorial").css("display", "block");
+	});
+	$("#equipe").on("click", function(){
+		$("#sobre").css("display", "block");
+	});
+	// Ate aqui finaliza os codigos menu 
+
+
 function adicionar(id, color){
 	if (color == "red") {
 		var contador = 5;
